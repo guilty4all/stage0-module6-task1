10c6b252561a7a3464abbce06599af55c6c66e9d
guilt <gulilty4all@gmail.com>
2022-09-14 16:18:41 +0600

    animal solution

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,30 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
 
+    public String getDescription (){
+        String resultString = "";
+        String paw = "";
+        String fur = "";
+        if(numberOfPaws == 1){
+            paw = "paw";
+        }else{
+            paw = "paws";
+        }
+        if(hasFur == true){
+            fur = "a";
+        }else{
+            fur = "no";
+        }
+        resultString += "This animal is mostly "+ color+"."+"It has "+numberOfPaws+" "+paw+" and "+fur+" fur.";
+        return resultString;
+    }
 }
