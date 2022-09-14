09004f2c1c4136f1d3b2fb84cbc3bc1bff9d5cce
guilt <gulilty4all@gmail.com>
2022-09-14 16:55:38 +0600

    fixes

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -10,7 +10,7 @@ public class Bird extends Animal{
     @Override
     public String getDescription(){
         String childresult = super.getDescription();
-        childresult += "Moreover, it has 2 wings and can fly.";
+        childresult += " Moreover, it has 2 wings and can fly.";
         return childresult;
     }
 }
